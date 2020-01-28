eedcd2ce4dd7542ecb156759bb7391ae6e9801ed
Marco <msilletti@aliceposta.it>
2020-01-28 23:29:29 +0100

    Update Version 2

---
+++ FootballDataService/Interfaces/IFootballData.cs
@@ -0,0 +1,15 @@
+﻿using FootballDataService.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FootballDataService.Interfaces
+{
+    public interface IFootballData
+    {
+        List<Competition> GetCompetitions();
+        Competition GetCompetition(int id);
+    }
+}
